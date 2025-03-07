95b3104265bb4b82aa95e48018751a563a4ae6af
benishaBM <benisha.b@annulartechnologies.com>
2025-03-07 15:18:34 +0530

    bug fixed

--- HealthCare/src/main/java/com/annular/healthCare/service/serviceImpl/AuthServiceImpl.java
+++ HealthCare/src/main/java/com/annular/healthCare/service/serviceImpl/AuthServiceImpl.java
@@ -15,6 +15,7 @@ import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -47,6 +48,9 @@ public class AuthServiceImpl implements AuthService {
 	
 	@Autowired
 	HospitalAdminRepository hospitalAdminRepository;
+	
+	@Value("${annular.app.imageLocation}")
+	private String imageLocation;
 
 	@Override
 	public ResponseEntity<?> register(UserWebModel userWebModel) {
@@ -79,6 +83,7 @@ public class AuthServiceImpl implements AuthService {
 
 			// Save user
 			User savedUser = userRepository.save(newUser);
+			
 
 			return ResponseEntity.ok(new Response(1, "success", "User registered successfully"));
 
