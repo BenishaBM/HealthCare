ab29b4e439ad100f0b44037dbd82b885be02c402
benishaBM <benisha.b@annulartechnologies.com>
2025-03-26 15:57:55 +0530

    Added missing Files

---
+++ HealthCare/src/main/java/com/annular/healthCare/webModel/DoctorLeaveListWebModel.java
@@ -0,0 +1,23 @@
+package com.annular.healthCare.webModel;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class DoctorLeaveListWebModel {
+	
+    private Date doctorLeaveDate;
+    private Boolean userIsActive;
+
+}
