ba4446ee8fcfb9ffaaf596919a18d596da342568
benishaBM <benisha.b@annulartechnologies.com>
2025-03-27 11:07:03 +0530

    bug fixed

--- HealthCare/src/main/java/com/annular/healthCare/security/WebSecurityConfig.java
+++ HealthCare/src/main/java/com/annular/healthCare/security/WebSecurityConfig.java
@@ -76,7 +76,7 @@ public class WebSecurityConfig {
                 .and()
                 .authorizeRequests(
                         (authorize) -> authorize
-                                .antMatchers("/auth/login","/auth/refreshToken","/user/login","/user/refreshToken","/patientDetails/register")
+                                .antMatchers("/auth/login","/auth/refreshToken","/user/login","/user/refreshToken","/patientDetails/register","/user/getByHopitalName","/user/getHospitalDataByUserTypeAndHospitalId")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
