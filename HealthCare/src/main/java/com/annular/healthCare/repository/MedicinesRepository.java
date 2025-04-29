6f045b10d47983e7f50b6aa1df32428df8328e16
Benisha BM <benisha.b@annulartechnologies.com>
2025-04-29 10:36:44 +0530

    bug fixed

--- HealthCare/src/main/java/com/annular/healthCare/repository/MedicinesRepository.java
+++ HealthCare/src/main/java/com/annular/healthCare/repository/MedicinesRepository.java
@@ -3,6 +3,7 @@ package com.annular.healthCare.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.annular.healthCare.model.Medicines;
@@ -10,6 +11,7 @@ import com.annular.healthCare.model.Medicines;
 @Repository
 public interface MedicinesRepository extends JpaRepository<Medicines,Integer>{
 
+	@Query("SELECT m FROM Medicines m WHERE m.hospitalId = hospitalId AND m.isActive = true")
 	List<Medicines> findByHospitalIdAndIsActiveTrue(Integer hospitalId);
 
 }
