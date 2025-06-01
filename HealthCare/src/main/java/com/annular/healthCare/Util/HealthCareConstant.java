fc9ee020ad108e84e556e2deb626a303f935962d
Benisha BM <benisha.b@annulartechnologies.com>
2025-06-01 17:46:40 +0530

    missed files

--- HealthCare/src/main/java/com/annular/healthCare/Util/HealthCareConstant.java
+++ HealthCare/src/main/java/com/annular/healthCare/Util/HealthCareConstant.java
@@ -4,8 +4,9 @@ public class HealthCareConstant {
 	
 	public static final int hospitalLogo = 1;
 	public static final int ProfilePhoto = 2;
-	public static final int patient = 3;
+	public static final int patientDocument = 3;
 	public static final int scanDocument = 4;
 	public static final int resultDocument = 5;
+	
 
 }
